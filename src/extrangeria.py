0b766eae176fcd8da6983ccf01febef5fd515800
GHJ5368 <jesantbar@alum.us.es>
2024-11-25 12:35:57 +0100

    modified:   src/extrangeria.py modified:   src/extrangeria_test.py

--- src/extrangeria.py
+++ src/extrangeria.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from collections import defaultdict
 import csv
 
 RegistroExtranjeria = namedtuple('RegistroExtranjeria', 'distrito,seccion,barrio,pais,hombres,mujeres')
@@ -36,3 +37,82 @@ def total_extranjeros_por_pais(registros):
             res[r.pais] = r.hombres + r.mujeres
 
     return sorted(res.items(), key= lambda x:x[1], reverse=True)
+
+def top_n_extranjeria(registros, n=3):
+    extrangeros_por_pais = total_extranjeros_por_pais(registros)
+    return extrangeros_por_pais[:n]
+
+def total_paises_por_barrio(registros):
+    res = defaultdict(set)
+    for r in registros:
+        res[r.barrio].add(r.pais)
+    '''
+    si fuera un diccionario normal:
+
+    if r.pais in res:
+        res[r.barrio].add(r.pais)
+    else:
+        res[r.barrio] = set( [r.pais] )
+    '''
+
+    return res
+
+def barrio_mas_multicultural(registros): 
+    paises_por_barrio = total_paises_por_barrio(registros)
+    
+    return max( paises_por_barrio.items(), key= lambda x: len(x[1]) )[0]
+    # return max( paises_por_barrio, key= lambda x: len( paises_por_barrio.get(x) ) )
+
+def barrio_con_mas_extranjeros(registros, tipo=None):
+    res = defaultdict(int)
+    
+    for r in registros:
+    
+        if tipo is None:
+            res[r.barrio] += r.hombres + r.mujeres
+
+        elif tipo.lower() == "hombres":
+            res[r.barrio] += r.hombres 
+        
+        elif tipo.lower() == "mujeres":
+            res[r.barrio] += r.mujeres
+    
+    '''
+
+    ########## OTRA FORMA ##########
+
+    if tipo is None:
+        
+        for r in registros:
+            res[r.barrio] += r.hombres + r.mujeres
+    
+    elif tipo.lower() == "Hombres":
+        
+        for r in registros:
+            res[r.barrio] += r.hombres 
+    
+    elif tipo.lower() == "Mujeres":
+        
+        for r in registros:
+            res[r.barrio] += r.mujeres
+    '''
+    return max( res, key= lambda x: res.get(x) )
+    
+def registros_por_distrito(registros):
+    res = defaultdict(list)
+    for r in registros:
+        res[r.distrito].append(r)
+
+    return res
+
+def pais_mas_representado_por_distrito(registros):
+    registros_agrupados = registros_por_distrito(registros)
+    resultado = defaultdict(str)
+
+    for clave, valor in registros_agrupados.items():
+        pais = max(total_extranjeros_por_pais(valor), key= lambda x: x[1])[0]
+        resultado[clave] = pais
+
+    return resultado
+
+
--- src/extrangeria_test.py
+++ src/extrangeria_test.py
@@ -12,9 +12,36 @@ def test_secciones_distritos_con_extranjeros_nacionalidades(registros, paises):
 def test_total_extranjeros_por_pais(registros):
     print(total_extranjeros_por_pais(registros))
 
+def test_barrio_mas_multicultural(registros):
+    resultado = barrio_mas_multicultural(registros)
+    print("TEST DE LA FUNCIÓN barrio_mas_multicultural:")
+    print(f"El barrio más multicultural de sevilla es {resultado}")
+    print("##############################################################")
+
+def test_barrio_con_mas_extranjeros(registros, tipo= None):
+    resultado = barrio_con_mas_extranjeros(registros, tipo)
+    print("TEST DE LA FUNCIÓN barrio_con_mas_extranjeros:")
+    print(f"El barrio con más residentes extranjeros es {resultado}.")
+    print(f"El barrio con más hombres residentes extranjeros es {resultado}.")
+    print(f"El barrio con más mujeres residentes extranjeras es {resultado}.")
+    print(f"##############################################################")
+
+def test_pais_mas_representado_por_distrito(registros):
+    resultado = pais_mas_representado_por_distrito(registros)
+    print("TEST DE LA FUNCIÓN pais_mas_representado_por_distrito:")
+    print("Los países con más residentes en cada distrito son los siguientes:")
+    for clave, valor in resultado.items():
+        print(f"Distrito: {clave} => {valor}")
+    
+    print("##############################################################")
+
 
 if __name__ == "__main__":
     datos = lee_datos_extranjeria("LAB-Extranjeria\data\extranjeriaSevilla.csv")
     #test_lee_datos_extranjeria(datos)
-    test_secciones_distritos_con_extranjeros_nacionalidades(datos, ["ALEMANIA","ITALIA"])
-
+    #test_secciones_distritos_con_extranjeros_nacionalidades(datos, ["ALEMANIA","ITALIA"])
+    #test_barrio_mas_multicultural(datos)
+    #test_barrio_con_mas_extranjeros(datos, tipo= None)
+    #test_barrio_con_mas_extranjeros(datos, tipo= "Hombres")
+    #test_barrio_con_mas_extranjeros(datos, tipo= "Mujeres")
+    test_pais_mas_representado_por_distrito(datos)
